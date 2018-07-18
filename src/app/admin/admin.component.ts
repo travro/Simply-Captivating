9b9872231cf8f1ae07dd8381becb4df0c12582fb
Travis Melvin <travro25@gmail.com>
2018-07-18 17:50:23 -0400

    new components

---
+++ src/app/admin/admin.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "../../../node_modules/@angular/core";
+
+
+@Component({
+    moduleId: module.id,
+    templateUrl: "admin.component.html"
+})
+export class AdminComponent {}
