04104bcf21b24608527c2a786ba5a9d56f7acd43
Travis Melvin <travro25@gmail.com>
2018-07-18 11:46:38 -0400

    added rest data source

---
+++ src/app/model/rest.datasource.ts
@@ -0,0 +1,42 @@
+import { Injectable } from "@angular/core";
+import { Http, Request, RequestMethod } from "@angular/http";
+import { Observable } from "rxjs/Observable";
+import { Product } from "./product.model";
+import { Cart } from "./cart.model";
+import { Order } from "./order.model";
+import "rxjs/add/operator/map";
+
+const PROTOCOL = "http";
+const PORT = 3500;
+
+@Injectable()
+export class RestDataSource {
+
+    baseUrl: string;
+    constructor(private http: Http) {
+        this.baseUrl = `${PROTOCOL}://${location.hostname}:${PORT}/`;
+    }
+
+    getProducts(): Observable<Product[]> {
+        return this.sendProductRequest(RequestMethod.Get, "products");
+    }
+
+    saveOrder(order: Order): Observable<Order> {
+        return this.sendOrderRequest(RequestMethod.Post, "orders", order);
+    }
+
+    private sendProductRequest(verb: RequestMethod, url: string): Observable<Product[]> {
+        return this.http.request(new Request({
+            method: verb,
+            url: this.baseUrl + url
+        })).map(response => response.json());
+    }
+
+    private sendOrderRequest(verb: RequestMethod, url: string, body: Product | Order): Observable<Order> {
+        return this.http.request(new Request({
+            method: verb,
+            url: this.baseUrl + url,
+            body: body
+        })).map(response => response.json());
+    }
+}
--- src/app/model/static.datasource.ts
+++ src/app/model/static.datasource.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Product} from './product.model';
+import { Product } from './product.model';
 import { Observable } from 'rxjs/Observable';
 import "rxjs/add/observable/from";
 import { Order } from './order.model';
@@ -9,137 +9,125 @@ import { Order } from './order.model';
 
 export class StaticDataSource {
 
-	private products : Product[] = [
+	private products: Product[] = [
 
-		new Product(		
-			"Skin", 
-			"3 in 1 Cleanser", 
+		new Product(
+			"Skin",
+			"3 in 1 Cleanser",
 			"3-in-1 cleanser designed and formulated to clean, tone, and exfoliate in one easy step",
-			"https://lh3.googleusercontent.com/pAzAwoVf7-ZlVRYIiGA_HSlNU_aykYD5NyCIS-A7U-nDtyt2YsrmE70JNQEo6_TKUjE2AqvZodk0UZOHH5Rb2iaLOyKotFF0dvK_13RXg8SL2S4G0XQM5b8YY5qKmNu1fOXtNYUhPw=w2400" , 
-			30),
+			"https://lh3.googleusercontent.com/pAzAwoVf7-ZlVRYIiGA_HSlNU_aykYD5NyCIS-A7U-nDtyt2YsrmE70JNQEo6_TKUjE2AqvZodk0UZOHH5Rb2iaLOyKotFF0dvK_13RXg8SL2S4G0XQM5b8YY5qKmNu1fOXtNYUhPw=w2400",
+			3),
 		new Product(
 			"Skin",
-			"DayTime Moisturizer", 
+			"DayTime Moisturizer",
 			"Daily moisturizer that provides environmental protection in a unique formula that meets the precise needs of your skin type",
-			"https://lh3.googleusercontent.com/1PT57EgnlspGlQ7tiUftjkS_zop6NKYspVTLJgACtC0yQvZuoYEZXTNxZTSS_J2xlH9iIhmjVxEgTBh4oFuEC72egNQu4TtQB0HkiOfIBgfu4p0x9FuT-3i0krSVyfbCDFjnZpkVUw=w2400", 
-			50),
+			"https://lh3.googleusercontent.com/1PT57EgnlspGlQ7tiUftjkS_zop6NKYspVTLJgACtC0yQvZuoYEZXTNxZTSS_J2xlH9iIhmjVxEgTBh4oFuEC72egNQu4TtQB0HkiOfIBgfu4p0x9FuT-3i0krSVyfbCDFjnZpkVUw=w2400",
+			5),
 		new Product(
 			"Skin",
-			"Evening Moisturizer", 
+			"Evening Moisturizer",
 			"Rich moisturizer that allows skin the opportunity to repair, re-moisturize, and rejuvenate itself while you sleep",
-			"https://lh3.googleusercontent.com/SZGUMs8aJeqe9kaIlr_VPpnMn86UMk_SQuLDyeB0LK3vC7H8zP4EKm1aP73ijJAx9r5CPtzepDYI2mmnQSwlacOMoOdH35eyzf9E5HReYobkc_OAGX6Ssy3912UE1uxvukrYFrdncw=w2400", 
-			60),
+			"https://lh3.googleusercontent.com/SZGUMs8aJeqe9kaIlr_VPpnMn86UMk_SQuLDyeB0LK3vC7H8zP4EKm1aP73ijJAx9r5CPtzepDYI2mmnQSwlacOMoOdH35eyzf9E5HReYobkc_OAGX6Ssy3912UE1uxvukrYFrdncw=w2400",
+			6),
 		new Product(
 			"Skin",
-			"Climate Control", 
+			"Climate Control",
 			"An anti-aging moisturizer for sensitive and dry skin types made up of a proprietary formulation of two separate and extremely effective delivery systems, combined into one advanced technology",
-			"https://lh3.googleusercontent.com/fR5Gi5PolCsIzn3p1Sc5JDCPUpzUzz9YBmSOOveKxlPlPQ0ZfXVVbnl9ODyD0n64CUYxP_GUqwJOd7xk_EfeDsyhwptt8CAHpKfu9wHGM0rrz9FAbEYzk_q6UmICAb0zLeM0uaT79A=w2400", 
-			60),
+			"https://lh3.googleusercontent.com/fR5Gi5PolCsIzn3p1Sc5JDCPUpzUzz9YBmSOOveKxlPlPQ0ZfXVVbnl9ODyD0n64CUYxP_GUqwJOd7xk_EfeDsyhwptt8CAHpKfu9wHGM0rrz9FAbEYzk_q6UmICAb0zLeM0uaT79A=w2400",
+			6),
 		new Product(
 			"Skin",
-			"SeneSerum-C", 
+			"SeneSerum-C",
 			"A 100% natural blend of the most advanced anti-aging ingredients in an ‘urbanization’ defense formula for all skin types. So light, it can be used night and day under moisturizers",
-			"https://lh3.googleusercontent.com/BUXcfy1aX3k5b4DApL9Pm-AAvFYpUrpFyWtxX39ZRHB5Cy25Va6KHI7ehRUQzlcM5AETk5anbv-MgHZf58qZqg2HooUctlkthTrYpeKYkf9Et1jwt3YzEBLDI-gGd4vQ7zXnv4LiMA=w2400", 
-			65),
+			"https://lh3.googleusercontent.com/BUXcfy1aX3k5b4DApL9Pm-AAvFYpUrpFyWtxX39ZRHB5Cy25Va6KHI7ehRUQzlcM5AETk5anbv-MgHZf58qZqg2HooUctlkthTrYpeKYkf9Et1jwt3YzEBLDI-gGd4vQ7zXnv4LiMA=w2400",
+			6),
 		new Product(
-			"Skin", 
-			"EyeCrème", 
+			"Skin",
+			"EyeCrème",
 			"A velvety-smooth crème that works to improve and protect the condition of skin around the eye, where skin first becomes wrinkled due to the constant movement",
-			"https://lh3.googleusercontent.com/FovJ0c65Kk8kt2eA8TMVtd5TpvCjaWfK4kYZx405nr4kKjgVfO0tEMlMC_EHwvtefAHvxVbulVMiygX8-m4J3Eo88xHj42ibThxIjVWoZttHpB2OHxRVVeypj22g_UvO4V4XIgllZQ=w2400", 
-			50),
+			"https://lh3.googleusercontent.com/FovJ0c65Kk8kt2eA8TMVtd5TpvCjaWfK4kYZx405nr4kKjgVfO0tEMlMC_EHwvtefAHvxVbulVMiygX8-m4J3Eo88xHj42ibThxIjVWoZttHpB2OHxRVVeypj22g_UvO4V4XIgllZQ=w2400",
+			5),
 		new Product(
 			"Skin",
 			"EyeLuminator",
 			"The perfect blend of SeneDerm® EyeCrème and SeneDerm® MakeSense Foundation® Pearlizer made to help make your skin more beautiful around the eye area – both inside and out",
 			"https://lh3.googleusercontent.com/dUybKsWl5P6SjonA3U2i7v6BZto-RNqZSLT8wm-FLWXYwflIPKNlokQURzM3qS8jSL00aQDZ46LiDrCbhmBDJXDiicmTcXQ8Yq49O2wS1jiX21Q0hIPk-coV9I4k2mWkCPIfqcUF_w=w2400",
-			50),
+			5),
 
 		new Product(
 			"Skin",
 			"Collagen Night Pak",
 			"A break-through anti-aging sleep treatment product that harnesses the power of 100% natural marine collagen, revitalizing caffeine, and SeneGence® proprietary anti-aging complex, SenePlex Complex™ to create a protective veil and seal in moisture while you sleep",
 			"https://lh3.googleusercontent.com/IeeSYL8k2xKCDv3j8Y29gqXVHvbIoPCGPgqWV4NSOHfIELfvJszUrsmFZNu--pogTjcxYN5uZffV4XwNRgqy3UGVP60QmtyN9RAuwNv1ujIe7XZ6OrfDopuPUspsqakDZVRz3Rhsfw=w2400",
-			85),
+			8),
 
 		new Product(
 			"Eyes",
 			"ShadowSense® Crème to Powder Eyeshadow",
 			"Experience long-lasting, rich eye shadow shades that start as a crème and finish as a powder on the eyes. Shades can be worn separately or blended to create new colors and looks",
 			"https://lh3.googleusercontent.com/eQnGMJEiOJtWY0K7_2X1P0D3eGSfQiabNLK3QeFd7EqBp1GboXpi1mBWEuZ3GvkjDMFIG8pO5d-sXtN2kOxHSv2eRFgTkvLXduirrpGadx1JBiXU6cOot-66grLd7zXDxSBfuUnbBQ=w2400",
-			22
-			),		
+			2),
 		new Product(
 			"Eyes",
 			"BrowSense® Liquid Brow Color",
 			"Perfect the shape of your brows with this unique, waterproof liquid brow color",
 			"https://lh3.googleusercontent.com/UFnxbHII9b8QYyRH1ARUcMtYMYyItBs08yfcBreZCIEPWrpP3P5Bjuwp3rLgWG7_K6aPndb76UW1CVBnVAaKS8QDkNKcNzdXd2ERy0twzDKFBFle74YxLI8C2wOrJRmUIbsH7DAI4A=w2400",
-			20
-		),
+			2),
 		new Product(
 			"Eyes",
 			"UnderSense® Lash Builder",
 			"Before applying LashSense® mascara, coat your lashes with this effective lengthener and builder",
 			"https://lh3.googleusercontent.com/SAxIflDzm3DVYwcNyXiBTm_V4e4rQzcfwh_7_7yzFl2gr3VBRbf114zGA3tPSpAdftlzoFrqRydwpxXYapPxzcaQ1rnS36uto0UfNFR5o_rYNGM26jOBVfsCfm-C-0Zjb54cZL4aag=w2400",
-			20
-		),
+			2),
 		new Product(
 			"Eyes",
 			"LashSense Waterproof Mascara",
 			"Brush on richly pigmented color to your lashes and be amazed that they are evenly coated, separated, volumized and shaped",
 			"https://lh3.googleusercontent.com/HzAoi6BbV5yiVvAm2JTubMDyiDhwB6XrdM4lN-p_1tSgREkmSDUq79CHVn4YuKmUQUWM4qkPS7lcS0yQ2d6SRCS57rdeMQjaZcumawGhm2uXZTLZU38KbzoUjqj0qwft1u0DgxSy_g=w2400",
-			25
-		),
+			2),
 		new Product(
 			"Eyes",
 			"LashSense Mascara with UnderSense Lash Builder",
 			"Waterproof LashSense® Mascara on one side, and lengthening and building UnderSense on the other makes for a perfect combination",
 			"https://lh3.googleusercontent.com/s_wDD5GNwH7vmY0II5lxmb1Rgngvnqa9FpNau7aJRvpkFNl9Gu6G4S0gdaALa_cyU3C_XCdwbfGm06hEfoc3cNzFoM4z-2_iuFCj-Li7a8bhbimvLLbvaAhIzbzHkWGUoBLrGbzgAA=w2400",
-			20
-		),
+			2),
 		new Product(
 			"Eyes",
 			"EyeSense Liquid Eye Liner",
 			"Transform your eye makeup look by effortlessly creating crisp, flawless eye liner shapes that last all day",
 			"https://lh3.googleusercontent.com/6GEhcLeOH8ba-s_Bm2Y69Ui8rLKupV9RBa-SoT6I6OxGPO2GMWtJ-VYKEZ-FHJjqCkjSH_JXPBy4x02u_vD1U03c5HyXaV08wCFEOjxtmtLhrwqcA4TtKnJ1JksytjEonYSpji4VSA=w2400",
-			25
-		),
+			2),
 		new Product(
 			"Eyes",
 			"LashExtend",
 			"Don't battle with false eyelashes or expensive extensions when you can naturally enhance your gorgeous gaze. Simply apply LashExtend to the base of the lashes and watch your lashes extend in length. Colored liquid liners are perfect for brilliantly lined eyes and luscious lashes in one easy step",
 			"https://lh3.googleusercontent.com/TvrnIS7WkW9o30YfL1Qg0qzUqnbxHgHhQheseudi0OLDrW3e3W6hNnZw4vV1ScVWjhjFzJXTSwcENswVgF_zAhPqqZcYEJKQxwYkTWRLZn8QTOgVQbQb36L5tUdZMLHl8kbxoRtdGA=w2400",
-			50
-		),
+			5),
 		new Product(
 			"Eyes",
 			"EyeSense® Shimmer Liquid Eye Liner",
 			"Transform your eye makeup look by effortlessly creating shimmering, flawless eye liner shapes that last all day",
 			"https://lh3.googleusercontent.com/2Bqoo9EMAMsIiv74JKemLOKfIweDvOrXWyS74ojWvSYx1KXm4XeftHBehLDf429MZ3Sb15ww2EnJTLoTSC0stgtqWA1qpP1qTvjuQpZvoRAEuCvP6LB0bd_3BwNu4UC_EQdRfh0Zyg=w2400",
-			25
-
-		),
+			2),
 
 		new Product(
 			"Parfums",
 			"Abundance Fresh™",
 			"Fragrance conjures powerful reactions and memories. The three Abundance Parfums were created with much more in mind – they are also beneficial for your body. These modern, refreshing parfums contain our proprietary SenePlex® Complex. Enjoy a fragrant, lasting impression while improving your skin even more deeply with the combined anti-aging benefits of SenePlex Complex",
 			"https://lh3.googleusercontent.com/y1zaDFGM4pneAi-Eoco7yxKckMLj1bUjArVy_yzABgWqSkmGuBWL5TgMVqbnytQTs2Kl5stZOlZBVRyAHXuv9wx3k9GTcamtI05n-5onSRNOWEDrd_UyKv_SWM-EsgiwCydA8iEChA=w2400",
-			70
-		),
+			7),
 		new Product(
 			"Parfums",
 			"Abundance Bloom™",
 			"Fragrance conjures powerful reactions and memories. The three Abundance Parfums™ were created with much more in mind – they are also beneficial for your body. These modern, refreshing parfums contain our proprietary SenePlex® Complex. Enjoy a fragrant, lasting impression while improving your skin even more deeply with the combined anti-aging benefits of SenePlex® Complex",
 			"https://lh3.googleusercontent.com/R8CnpIP2NeRrPS22OLndzmUwo724HOWO4xRc8C-JL3FBGACwiG2E44Uui_6GzGavzQ5mtpue2n0hobrVANJKd9yB9KmULnGzU1VK5ORcqxnug6z6YQeN2t-EB9JDD0CLczcSvt9uOQ=w2400",
-			70
-		),
+			7),
 		new Product(
 			"Parfums",
 			"Abundance Lush™",
 			"Fragrance conjures powerful reactions and memories. The three Abundance Parfums were created with much more in mind – they are also beneficial for your body. These modern, refreshing parfums contain our proprietary SenePlex® Complex. Enjoy a fragrant, lasting impression while improving your skin even more deeply with the combined anti-aging benefits of SenePlex® Complex",
 			"https://lh3.googleusercontent.com/ZNHPjtLrBvPi89PUVE_wOR03eag4qrYHNgi12Xz0faaPoqyt4XGwrK5hC6x9mkTee-k8ickmT4j7SMvgHffY4uPGoDfIq_pHqOr1wQNj-qIQCFjNR5mvqevlCnMycqlZKXIErzdfqA=w2400",
-			70
-		),
+			7),
 
 
 		new Product(
@@ -147,90 +135,90 @@ export class StaticDataSource {
 			"LipSense",
 			"The premier product for SeneGence®, LipSense (The Original Long-Lasting Lip Color™) is a patented, amazing departure from conventional lipsticks and lip colors. LipSense is versatile in that you can mix shades to create a number of effect",
 			"https://lh3.googleusercontent.com/hhOMetnvzPz7rQF-f7_tSwKBVbnD3PIlhZ4fbSzdiLZjovz2ECAU917O-khbSScL08DBXh1cw0Aeccn5GtLqVS5BAjFtEHVh6ouvxhYiHO_jaYdX8aj-vMZQSzCcRMmrQ8yoeYXhyQ=w2400",
-			25),
+			2),
 		new Product(
 			"Lips",
 			"LinerSense",
 			"Line your lips with your choice of shade, for a more defined and finished look. Includes fine, sable brush for accuracy when applying",
 			"https://lh3.googleusercontent.com/PhFBCfa7zvc8_NqzpfZqYCwHk39AKhO4dV7HoHDVft0LgMF1sBfcUnpN40SC0EbFh4jE3n_QOcSE7-D7AvZ-qVKG0cpk2XCo3Wug-KmUxF9cYf4UBPLqu7ETweQztgiPreawxsSQwA=w2400",
-			22),
+			2),
 		new Product(
 			"Lips",
 			"LipSense Gloss",
 			"Smooth on any texture of LipSense® Gloss to add rich moisture to lips, either on top of LipSense® Liquid Lip Color, or on bare lips. Gloss also helps to seal in LipSense® Lip Color and improve the longevity of the color",
 			"https://lh3.googleusercontent.com/BJCRqMSjOMavMP_GgXsaxzefC5KO19LDKPuJooW0YGdhZ8dHgGfGiytk4Hegj3tz_Xfsh0-zUk9TEnUO-FLRiuHddRvO3zmzRGcO2mSchDGSa2G6_YcnNnNJ8mw56fjPi5BaB761mg=w2400",
-			20),
+			2),
 		new Product(
 			"Lips",
 			"Lip Balm",
 			"Smooth on any texture of LipSense® Gloss to add rich moisture to lips, either on top of LipSense® Liquid Lip Color, or on bare lips. Gloss also helps to seal in LipSense® Lip Color and improve the longevity of the color",
 			"https://lh3.googleusercontent.com/mDCHH_G07rtNLR5duIAjXhYEs-Asa4reemmT4TeXa69SzuHtuluDVfLJg2PRovQybMP6iYYIUdwIIN4-aq9e8vYiZVO-XfP1YhggwZ2HqxOhxDydkv5Hyze5MnGt4O6jjIEJRPyJDA=w2400",
-			20),
+			2),
 		new Product(
 			"Lips",
 			"Oops Remover",
 			"Specially designed to erase those mistakes that can occur when applying LipSense lip color and will lift your LipSense color to change your shade or remove at the end of the day",
 			"https://lh3.googleusercontent.com/A149KHf1-6rRxCQGN3sbO_UEo2bD0InYDd-egvNZsQhWp8vXoE9VZNqlyetGz1xplh7fu9pLcrlyhZXI311wEBvPxQDkGnJ-uAbP57LJSUc1SO4--Ss37fiZcumu9DACm7ZJ6tcY-A=w2400",
-			10),
+			1),
 
 		new Product(
 			"BodyCare",
 			"Smoothing Body Scrub",
 			"Help make skin on your body beautiful, soft and smooth with this gently but highly effective body exfoliator",
 			"https://lh3.googleusercontent.com/ET6i15lDZlPqQoR9gQfWU9hj8-xhvjpeRckPs7yue7_dBazEWdrnP-u9jWEbjK35UqToZL5d63ckQi00Cy7gEXOcFT3YvzRusR0wWZpU1ziaE85OgJhwy4z8ysDUsLhNemSLbP120g=w2400",
-			20),
+			2),
 		new Product(
 			"BodyCare",
 			"Moisturizing Body Wash",
 			"This gentle, rich foaming gel works to cleanse and moisturize the entire body. While traditional soaps can dry skin, Body Wash conditions and restores pH balance without stripping",
 			"https://lh3.googleusercontent.com/enmQ2Twn8IOL0kct8CEpbLb0JlMHy9gwR-jeZSCOmjdzNnr5kPakMmzzK_Lp1MrAauFm-4xo4-er4uYiYRDcYT3ER-hAqmTGJW8rGMdcFIJvErpApu_IT4Uu2cFl9ACL2DBYJpJ-ug=w2400",
-			20),
+			2),
 		new Product(
 			"BodyCare",
 			"Advanced Hydration Body Lotion",
 			"The finest in skin care for your body – this rich cream delivers intense, advanced moisturization and anti-aging benefits from SenePlex® Complex+",
 			"https://lh3.googleusercontent.com/84zI3Y47vN50w-8fhBA8TfLd0nNGowOAC6yJtrtJMNos3kykOG_t-2lWBPFBF_RyDyJNkkoNXzV7GRp1F0JMghvllm4wEnUmd2QCRsDq-qf86FUanRmbjsrY4FToEH-y_XcFqDMe5A=w2400",
-			45),
+			4),
 		new Product(
 			"BodyCare",
 			"Hand Cream with Shea Butter",
 			"This rich hand moisturizer is packed with emollient rich Shea Butter to soothe and botanicals to protect skin while leaving no greasy feeling behind",
 			"https://lh3.googleusercontent.com/qFS_moFElWTLEAa4q1PPuQKyvxsnqACyXNgaOIZZP1mpCoNc9dvwm6hlFPNUtSjh3vY3C_6ykRKtdvaw3BoKZittFR_EXeZV3kA0YSFRm6DV5wk9fyEDR_5MI6tLTtd1TNqqvfZk8w=w2400",
-			20),
+			2),
 		new Product(
 			"BodyCare",
 			"Detoxifying & Moisturizing Mask",
 			"This creamy, non-drying natural mineral and botanical-based treatment mask gently removes impurities from pores to reveal smoother and softer-looking skin",
 			"https://lh3.googleusercontent.com/lXw3q2yvhBeuYXsc1wl0wT1Sp56awb-jvxiWoxzGsyIYPc5tXMA_lQb2ZmfgX5bUTpdUP9wVpcxdzt_bJ05hNLadKgU_W7gwK7dYa8rfvPLEC_adIpv_49OcmERNaJoKsFMX8LFiHw=w2400",
-			50),
+			5),
 		new Product(
 			"BodyCare",
 			"Shea Butter Body Cream",
 			"Let the natural healing properties of Shea Butter allow you to experience immediate dry skin relief with each application of this rich body moisturizer",
 			"https://lh3.googleusercontent.com/-i-i8mQTsjxK3q_ZohIAzVjb2BK4Uzt-cn_C1fBjsHRkBWSLDscWXrWsrkGxzqUx9dOWdtYVZ0wnVJB3jAeP5qqZUqdSggoDlBFLRJ7eQN0uf2rvmCjyG1lPfEcnlzRwe3FUDO0FMw=w2400",
-			30),
+			3),
 
 		new Product(
 			"SunCare",
 			"Self-Tanning Bronzing Coconut Milk",
 			"Get a gorgeous, natural-looking golden tan with this unique spritz-on self-tanner that contains effective ingredients and moisturizers for a fabulous tan and anti-aging protection in one product",
 			"https://lh3.googleusercontent.com/MQldHsL0Mg-lbtt9u5TlJX_Jpvpns-xIS7W69g-kOrR6dfxyGn2Dc94kQxyuuI5pAVImYLVMmPdFxIhdVt7KqhT5N8nQ2ecBRk3lio9Exgy23iGAaX2JXX2BDPzUpv0LNtKPrr7iAw=w2400",
-			50),
+			5),
 		new Product(
 			"SunCare",
 			"SeneSun SPF 30 SunScreen",
 			"Help protect your skin from the harmful rays of the sun with this light, SPF 30 sunscreen. SeneSun SunScreen is barely noticeable when applied as it does not feel thick, sticky, or oily. Mist on to defend your skin from UV rays while giving it the benefit of advanced anti-aging moisturizers and SenePlex Complex+",
 			"https://lh3.googleusercontent.com/MQldHsL0Mg-lbtt9u5TlJX_Jpvpns-xIS7W69g-kOrR6dfxyGn2Dc94kQxyuuI5pAVImYLVMmPdFxIhdVt7KqhT5N8nQ2ecBRk3lio9Exgy23iGAaX2JXX2BDPzUpv0LNtKPrr7iAw=w2400",
-			45)
+			4)
 	];
 
-    getProducts(): Observable<Product[]> {
-        return Observable.from([this.products]);
+	getProducts(): Observable<Product[]> {
+		return Observable.from([this.products]);
 	}
-	
-	saveOrder(order: Order) : Observable<Order> {
-        console.log(JSON.stringify(order));
-        return Observable.from([order]);
-    }
-    
- }
+
+	saveOrder(order: Order): Observable<Order> {
+		console.log(JSON.stringify(order));
+		return Observable.from([order]);
+	}
+
+}
